a95659f6e406723f17499c800e67c993cf8c1679
JaniduChathumina <86459858+JaniduChathumina@users.noreply.github.com>
2021-09-25 19:23:15 +0530

    Add files via upload

---
+++ q3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class q3
+{
+
+	public static void main (String args[])
+	{
+
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter your marks : ");
+	double mark = input.nextDouble();
+	if (mark<25) {System.out.print("F");}
+	if (mark>=25 && mark<45) {System.out.print("E");}
+	if (mark>=45 && mark<50) {System.out.print("D");}
+	if (mark>=50 && mark<60) {System.out.print("C");}
+	if (mark>=60 && mark<80) {System.out.print("B");}
+	if (mark>=80) {System.out.print("A");}
+	}
+
+}
+
+	
